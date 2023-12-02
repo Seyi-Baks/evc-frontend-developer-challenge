e4aee8d7416ca02eb081adf91070519764d02f75
seyib-dala <133716436+seyib-dala@users.noreply.github.com>
2023-12-02 15:46:13 +0000

    Implemented auth management using react context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './views/Login';
 import Dashboard from './views/Dashboard';
+import { AuthContextProvider } from './context/AuthContext';
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/signin' element={<LoginPage />} /> 
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/signin' element={<LoginPage />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   )
 }
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,59 @@
+import { GoogleAuthProvider, User, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { auth } from '../config/firebase'
+
+interface AuthContextType {
+    googleSignIn: () => Promise<void>;
+    logOut: () => void;
+    user: User | null;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+interface AuthContextProviderProps {
+    children: ReactNode;
+}
+
+export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    const googleSignIn = async () => {
+        const provider = new GoogleAuthProvider();
+        try {
+            await signInWithPopup(auth, provider);
+            //    signInWithRedirect(auth, provider)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const logOut = (): void => {
+        signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            console.log('User ', user)
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+
+    return (
+        <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export const UserAuth = (): AuthContextType => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthContextProvider');
+    }
+    return context;
+}
